0ab02498cc26366754a42046bb5dfc261195f6b2
eduardobosco <eduardoboscocs@gmail.com>
2020-09-09 12:43:32 -0300

    adicionado CSS

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+//contador de jogadas
+
+let player1 = 0;
+let player2 = 0;
+
+//adcionando o evento click aos boxes
+
+for (let i = 0; 1 < boxes.length; i++) {
+    //quando clica na caixa
+    boxes[i].addEventListener("click", function() {
+
+        let el;
+
+        if (player1 == player2) {
+            //x
+            el = x;
+        } else {
+            //o
+            el = o;
+        }
+
+
+
+    });
+}
